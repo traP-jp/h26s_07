238ada33576e338685f934f383e93ca18918b8db
Hokubu Subway <hokubusub@gmail.com>
2026-06-27 17:10:36 +0900

    AllMessages 内に PostMessage を含めるように構造を変更しました。

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <UApp>
-    <RouterView />
-  </UApp>
+  <AllMessages room-id="00000000-0000-0000-0000-000000000000" textarea />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AllMessages from './components/layouts/AllMessages.vue'
+</script>
--- frontend/src/components/layouts/AllMessages.vue
+++ frontend/src/components/layouts/AllMessages.vue
@@ -2,11 +2,7 @@
 import { ref } from 'vue'
 import type { Uuid, Message, DateTime } from '@/api/schema'
 const room = defineProps<{ roomId: string; textarea: boolean }>()
-const messages = ref<Message[]>([
-  { messageId: '6', content: 'Hello', author: { userId: 'HokubuRailway' }, createdAt: 'ima' },
-  { messageId: '61', content: '非常に長いメッセージを投稿したらどのような表示になるかを検証するための長文投稿です。', author: { userId: 'HokubuRailway' }, createdAt: 'ima' },
-  { messageId: '63', content: '', author: { userId: 'HokubuRailway' }, createdAt: 'ima' },
-])
+const messages = ref<Message[]>([])
 
 const addUserMessage = (m: Message) => {
   messages.value.push(m)
