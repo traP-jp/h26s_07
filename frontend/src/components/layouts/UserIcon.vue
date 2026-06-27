bb9364338dc3c086f0e31f392f2baedd9fcb6c6e
Hokubu Subway <hokubusub@gmail.com>
2026-06-27 15:29:44 +0900

    Resurrection of UserIcon.vue

---
+++ frontend/src/components/layouts/UserIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+const userId = defineProps<{
+  userId: string
+}>()
+const url = computed(() => `https://q.trap.jp/api/v3/public/icon/${userId.userId}`)
+</script>
+
+<template>
+  <div>
+    <img class="icon" :src="url" />
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+</style>
